d508b36bffccb07344548a9ddd630d4bd3da3146
AnaSofiR <asrodriguo@eafit.edu.co>
2026-04-18 08:45:10 -0500

    SonarCloud issues correction

--- app/app.py
+++ app/app.py
@@ -1,4 +1,7 @@
 # app/app.py
+"""
+Módulo app.py
+"""
 from flask import Flask, render_template, request
 from .calculadora import sumar, restar, multiplicar, dividir
 
@@ -7,6 +10,9 @@ app = Flask(__name__)
 
 @app.route("/", methods=["GET", "POST"])
 def index():
+    """
+    Método main
+    """
     resultado = None
     if request.method == "POST":
         try:
@@ -33,4 +39,5 @@ def index():
 
 
 if __name__ == "__main__":  # pragma: no cover
-    app.run(debug=True, port=5000, host="0.0.0.0")  # Quita debug=True para producción
+    app.run(debug=True, port=5000, host="127.0.0.1") 
+    # Quita debug=True para producción
--- app/calculadora.py
+++ app/calculadora.py
@@ -1,21 +1,39 @@
 # app/calculadora.py
+"""
+Módulo calculadora.py
+"""
 
-AUTORES = "jsocampod@eafit.edu.co, asrodriguo@eafit.edu.co, jdgutierrg@eafit.edu.co, mgutierrej@eafit.edu.co"  # IMPORTANTE: Reemplaza con los usuarios de correo de EAFIT de los estudiantes que participaron en la entrega, separados por comas.
+AUTORES = "jsocampod@eafit.edu.co, asrodriguo@eafit.edu.co," \
+" jdgutierrg@eafit.edu.co, mgutierrej@eafit.edu.co"  
+# IMPORTANTE: Reemplaza con los usuarios de correo de EAFIT de los
+#  estudiantes que participaron en la entrega, separados por comas.
 
 
 def sumar(a, b):
+    """
+    Método de suma
+    """
     return a + b
 
 
 def restar(a, b):
+    """
+    Método de resta
+    """
     return a - b
 
 
 def multiplicar(a, b):
+    """
+    Método de multiplicación
+    """
     return a * b
 
 
 def dividir(a, b):
+    """
+    Método de división
+    """
     if b == 0:
         raise ZeroDivisionError("No se puede dividir por cero")
     return a / b
